1beb71618d0e7720507df106644bdbba48faba4a
Dimitrios Krikelis <dimitriskrkls@gmail.com>
2020-10-14 00:43:14 +0300

    first script commit

---
+++ trackball_side.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+side_check=$(xinput list-props 'Logitech USB Trackball' | grep "286" | grep -o "[89]$")
+if [ $side_check == '8' ];then
+ swap_primary_button=$(xinput set-button-map 'Logitech USB Trackball' 3 2 1 ; xinput set-prop 'Logitech USB Trackball' 286 9)
+ echo -n "$swap_primary_button"
+ echo -e "\n\t<---Set to left handed.\n"
+else
+ swap_primary_button=$(xinput set-button-map 'Logitech USB Trackball' 1 2 3 ; xinput set-prop 'Logitech USB Trackball' 286 8)
+ echo -n "$swap_primary_button"
+ echo -e "\n\tSet to right handed.--->\n"
+fi
