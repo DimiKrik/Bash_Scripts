7da4dffc7e926d61956e952ae51fcfee7641872e
DimiKrik <47506009+DimiKrik@users.noreply.github.com>
2020-10-14 00:49:53 +0300

    Requires xinput

--- trackball_side.sh
+++ trackball_side.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Must be using libinput and requires xinput
 side_check=$(xinput list-props 'Logitech USB Trackball' | grep "286" | grep -o "[89]$")
 if [ $side_check == '8' ];then
  swap_primary_button=$(xinput set-button-map 'Logitech USB Trackball' 3 2 1 ; xinput set-prop 'Logitech USB Trackball' 286 9)
